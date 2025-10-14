f8a6f88150e65d0127392b7972f2abd465042d86
unknown <yfredj@outlook.com>
2025-10-14 13:08:22 +0100

    added in some comments and decisions about cause-effect

--- classes.py
+++ classes.py
@@ -1,10 +1,13 @@
 class particle():
-    def __init__(self, parents, children):
+    def __init__(self, parents, children, depth):
        self. parents = parents
        self.children = children
-       self.position = ()
+       self.depth =  depth
        self.velocity = (0, 0)
        self.accelaration = (0, 0)
     
     def move(self):
+        pass
+
+    def position(self,parents, children): # dijkstras to find the quikest path to an anchor
         pass
