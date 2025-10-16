47be5ca5d0833ada2d92a04c0117b6e3786eec7d
unknown <yfredj@outlook.com>
2025-10-16 09:07:18 +0100

    just a backup before i implement the locate function

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Demo
-
 --READ ME--
 
+This is currently a simulation that runs on my own algorithm which cascades all action up and down
--- classes.py
+++ classes.py
@@ -9,13 +9,22 @@ class particle():
        self.position = (x, y)
        self.velocity = velocity
        self.accelaration = acceleration    
-       
+
     def move(self):
         # if loses parent, siblings become parents
+
+        #need to make it so that anchors dont do this otherwise we cascade badly
+        if len(self.parents)==0: # if it loses its parents
+            self.parents=self.siblings.copy() # siblings become parents
+            self.siblings=self.children.copy() # children become siblings
+            self.children=[] #loses children
+            # {ADD FEATURE} make it so that the particle moves in a cirular path like a pedulum and makes the parent swing
+
         # if loses child, no impact (could have it bounce up but we can add that later)
         # if loses sibling, no impact (could have it swing a bit)
         # if particle below moves down, move current particle down depending on chain length (anchor-->tip)
         # if parent moves down, move current down by same amount
+        # if parent moves up, move current up depending on chain length
         pass
 
     def find(self,parents, children): 
